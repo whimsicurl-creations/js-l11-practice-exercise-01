a837624814acd535f54a500559e24227dc39b14c
Bethany <blmeier@gmail.com>
2021-08-17 15:37:16 -0400

    Show 5 random users with API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    //console.log(data);
+    const usersResults = data.results;
+    //console.log(usersResults);
+    displayUsers(usersResults);
+};
+
+getData();
+
+const displayUsers = function (userResults) {
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+};
